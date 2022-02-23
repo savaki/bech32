66fd9a24a0076ae306e3969a9ced8c0fedc27b85
Pi Lanningham <pi@sundaeswap.finance>
2022-02-23 22:02:23 +0000

    Add encoding to the command line interface

--- cmd/bech32/go.mod
+++ cmd/bech32/go.mod
@@ -13,3 +13,5 @@ require (
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 )
+
+replace "github.com/savaki/bech32" => "../../"
--- cmd/bech32/main.go
+++ cmd/bech32/main.go
@@ -12,10 +12,25 @@ import (
 
 var opts struct {
 	Format string
+	HRP    string
 }
 
 func main() {
 	app := cli.NewApp()
+	app.Commands = []*cli.Command{
+		{
+			Name:    "decode",
+			Aliases: []string{"d"},
+			Usage:   "decode a bech32 string to a human readable part and hex encoded data",
+			Action:  decode,
+		},
+		{
+			Name:    "encode",
+			Aliases: []string{"e"},
+			Usage:   "encode a human readable part and hex encoded data to bech32",
+			Action:  encode,
+		},
+	}
 	app.Usage = "parse bech32 encoded string"
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
@@ -25,7 +40,7 @@ func main() {
 			Destination: &opts.Format,
 		},
 	}
-	app.Action = action
+	app.Action = infer
 	err := app.Run(os.Args)
 	if err != nil {
 		fmt.Println(err)
@@ -33,7 +48,17 @@ func main() {
 	}
 }
 
-func action(c *cli.Context) error {
+func infer(c *cli.Context) error {
+	first := c.Args().First()
+	_, _, err := bech32.Decode(first)
+	if err != nil {
+		return encode(c)
+	} else {
+		return decode(c)
+	}
+}
+
+func decode(c *cli.Context) error {
 	var records []interface{}
 	for _, addr := range c.Args().Slice() {
 		hrp, data, err := bech32.Decode(addr)
@@ -51,6 +76,44 @@ func action(c *cli.Context) error {
 			})
 		default:
 			fmt.Printf("%v: %v %v\n", addr, hrp, hex.EncodeToString(data))
+		}
+	}
+
+	switch opts.Format {
+	case "json":
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.SetIndent("", "  ")
+		return encoder.Encode(records)
+	default:
+		return nil
+	}
+}
+
+func encode(c *cli.Context) error {
+	var records []interface{}
+	args := c.Args().Slice()
+	for i := 0; i < len(args); i += 2 {
+		hrp := args[i]
+		data, err := hex.DecodeString(args[i+1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "bech32: failed to encode data, %v: %v", data, err)
+			os.Exit(1)
+		}
+		addr, err := bech32.Encode(hrp, data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "bech32: failed to encode data, %v: %v", data, err)
+			os.Exit(1)
+		}
+
+		switch opts.Format {
+		case "json":
+			records = append(records, map[string]interface{}{
+				"hrp":  hrp,
+				"data": hex.EncodeToString(data),
+				"addr": addr,
+			})
+		default:
+			fmt.Printf("%v %v: %v\n", hrp, hex.EncodeToString(data), addr)
 		}
 	}
 
